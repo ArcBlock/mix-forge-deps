91bc69fe6bd967afdfd49a222eb494ffaf6310d8
Tyr Chen <tyr.chen@gmail.com>
2019-03-18 18:33:33 -0700

    add debug

--- .makefiles/elixir.mk
+++ .makefiles/elixir.mk
@@ -13,7 +13,7 @@ build-all:
 	@cd $(SRC); MIX_ENV=dev mix compile
 	@echo "Building dependencies for MIX_ENV=test..."
 	@cd $(SRC); MIX_ENV=test mix compile
-	@cd $(SRC); tar zcf builds.tgz _build/ deps/
+	cd $(SRC); tar zcf builds.tgz _build/ deps/; ls -l
 
 build-ubuntu: $(RELEASE_DIR) build-all build-version-file
 	mv $(SRC)/builds.tgz $(SRC)/ubuntu-builds.tgz
