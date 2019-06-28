81d7254180aaa6211d203bf18a54faac87292456
Boshan Sun <sunboshan@gmail.com>
2019-06-28 00:32:02 -0700

    Fix type

--- .makefiles/dep.mk
+++ .makefiles/dep.mk
@@ -5,4 +5,4 @@ TARGETS=centos-builds ubuntu-builds darwin-builds
 
 $(TARGETS):
 	@echo "Extracting $@ deps from mix-forge-deps repo $(DEPS_VER)..."
-	@cd $(SRC); wget --quite $(DEPS_PREFIX)/$(DEPS_VER)/$@.tgz; rm -rf _build/{staging,dev,test} deps; tar zxf $@.tgz; rm *.tgz || true
+	@cd $(SRC); wget --quiet $(DEPS_PREFIX)/$(DEPS_VER)/$@.tgz; rm -rf _build/{staging,dev,test} deps; tar zxf $@.tgz; rm *.tgz || true
